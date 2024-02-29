a07abb0ef463e04925192ab1d2890e4b346e94c2
phoenixbloomfield <phoenix.bloomfield@colorado.edu>
2024-02-29 06:50:43 -0700

    trying to push changes again

--- Firmware/HomeAir/settings.h
+++ Firmware/HomeAir/settings.h
@@ -20,31 +20,6 @@ struct display_settings{
   //UNIVERSAL SCREEN SETTINGS
   bool wallMounted = false;
   bool nightMode = false;
-<<<<<<< HEAD
-  bool clockEnabled = false;
-  uint8_t clockLocation = 0; // 0=top right
-  bool dotEnabled = false;
-  uint8_t dotLocation = 0; // 0=top right
-  uint16_t refreshTime = 1; // in seconds
-
-  //BURN IN PREVENTION
-  int cyclesBetweenFullRefresh = 300;
-  //number of refresh cycles between global refresh. Global refresh redraws entire screen to black and white several times to prevent burn in. Lower number here better prevents burn in but is more distracting. 
-  uint8_t numRefreshCycles = 5;
-  //number of times to flash the screen to prevent burn in. Does not need to be more than 10.
-
-  //LOGO SCREEN
-  uint8_t logoTime = 3; // time in seconds that Sparkfun logo is displayed upon boot
-
-  //PAIRING SCREEN
-
-  //SENSOR READINGS (NORMAL OPERATION)
-  uint8_t frame0sensor; // sensor on left
-  uint8_t frame1sensor; // sensor on right
-
-  //FIRMWARE UPDATE
-  uint8_t updatePercent; // update progress, should not be more than 100
-=======
   bool clockEnabled = true;
   bool dotEnabled = true;
   uint8_t clockLocation = 0;
@@ -52,7 +27,6 @@ struct display_settings{
   uint8_t frame1sensor;
   uint8_t frame2sensor;
   uint16_t refreshTime = 5;
->>>>>>> 76c14eb1af67f77d8ebcfc27f5f4c209fa706b38
 };
 
 extern display_settings epd_settings;
